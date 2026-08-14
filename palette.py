90239c245d6a0596fe8448041bc065ff6cce6210
shivainlabs <shiva.in.labs@gmail.com>
2026-08-14 06:09:02 +0530

    adding text_color according to image

--- palette.py
+++ palette.py
@@ -13,6 +13,9 @@ def create_color_palette(dominant_colors,palette_size=(400,60)):
         draw.rectangle([i * swatch_width,0,(i+1) * swatch_width, palette_size[1]], fill=tuple(color))
         
         hex_color = "#{:02x}{:02x}{:02x}".format(color[0],color[1],color[2])
-        draw.text((i*swatch_width + 10, 20),hex_color,fill='white')
+        
+        brightness = (0.299 * color[0] + 0.587 * color[1] + 0.114 * color[2])
+        text_color = 'black' if brightness > 128 else "white"
+        draw.text((i*swatch_width + 10, 20),hex_color,fill=text_color)
     
     return palette
